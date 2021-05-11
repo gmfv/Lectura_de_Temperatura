625fe68e37608443a6c97c6bf23175e0117597e2
ValenzuelaDiego <78625273+ValenzuelaDiego@users.noreply.github.com>
2021-05-11 00:47:32 -0400

    actualizaciones

--- SensorTemperatura1.sql
+++ SensorTemperatura1.sql
@@ -2,14 +2,14 @@
 -- medicionmedicion
 USE sensortemperatura1;
 -- 
--- DROP TABLE if EXISTS funcionario;
--- CREATE TABLE funcionario(
--- cedula VARCHAR(10),
--- nombre VARCHAR(50),
--- telefono VARCHAR(20),
--- mail VARCHAR(20),
--- PRIMARY KEY (cedula)
--- );
+DROP TABLE if EXISTS funcionario;
+CREATE TABLE funcionario(
+cedula VARCHAR(10),
+nombre VARCHAR(50),
+telefono VARCHAR(20),
+mail VARCHAR(20),
+PRIMARY KEY (cedula)
+);
 
 DROP TABLE if EXISTS medicion;
 CREATE TABLE medicion(
